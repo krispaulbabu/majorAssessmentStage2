764ce48c561368fc5800cbf968067451b49681f6
krispaulbabu <krispaulbabu.krispaulbabu@students.mq.edu.au>
2023-05-28 12:11:09 +1000

    best fit half working model

--- MyClient.java
+++ MyClient.java
@@ -1,5 +1,8 @@
+
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class MyClient {
 
@@ -14,27 +17,11 @@ public class MyClient {
 			// first handshake. Sending hello messages and other standard responses like
 			// Auth etc.,
 			dout.write(("HELO\n").getBytes());
-			// System.out.println((String) dis.readLine());
 			dis.readLine();
-			dout.flush();
 
 			String username = System.getProperty("user.name");
 			dout.write(("AUTH " + username + "\n").getBytes());
 			String lastmessage = (String) dis.readLine();
-			// System.out.println(lastmessage);
-			dout.flush();
-
-			// times represents the number of times the while loop has run. This variable is
-			// used to stop running the GETS command after one run of the while loop.
-			int times = 0;
-
-			// Initializing this variable inside the while loop messes it up by setting it
-			// to an empty string every time the loop runs. So I've kept it outside the
-			// loop.
-			String largestServer = "";
-
-			int numberOfLargeServers = 0;
-			int lastServerUsed = 0;
 
 			// This is where the job scheduling happens. Standard commands like REDY and
 			// GETS (which only runs once). Runs till there are no jobs left
@@ -42,8 +29,7 @@ public class MyClient {
 
 				dout.write(("REDY\n").getBytes());
 				String jobs = (String) dis.readLine();
-				// System.out.println(jobs);
-				dout.flush();
+				System.out.println(jobs);
 
 				// If the previous command yields NONE, the following lines of code will break
 				// the loop
@@ -55,77 +41,55 @@ public class MyClient {
 				String[] jobsIndivData = jobs.split(" ");
 				int jobId = Integer.parseInt(jobsIndivData[2]);
 
+				int neededCores = Integer.parseInt(jobsIndivData[4]);
+
 				// The following if statement checks if the code's already been run. In case it
 				// has not, It asks for server info and finds out the largest server
-				if (times < 1) {
 
-					dout.write(("GETS ALL\n").getBytes());
-					String data = dis.readLine();
-					dout.flush();
+				dout.write(
+						("GETS Capable " + jobsIndivData[4] + " " + jobsIndivData[5] + " " + jobsIndivData[6] + "\n").getBytes());
+				String data = dis.readLine();
 
-					dout.write(("OK\n").getBytes());
-					dout.flush();
+				System.out.println(data);
 
-					// Splits the data string to find the nRecs
-					String[] indivData = data.split(" ");
-					int nRecs = Integer.parseInt(indivData[1]);
+				dout.write(("OK\n").getBytes());
 
-					String servers = "";
-					String[][] serversList = new String[nRecs][];
-					int largestServerCores = 0;
+				int serversPresentNo = Integer.parseInt(data.split(" ")[1]);
 
-					// Prints out server information and finds out the largest server
-					for (int i = 0; i < nRecs; i++) {
-						servers = (String) dis.readLine();
-						// System.out.println(servers);
+				String serverRead = (String) dis.readLine();
 
-						String[] serverIndiv = servers.split(" ");
-						serversList[i] = serverIndiv;
+				String minServerName = serverRead.split(" ")[0];
+				int minServerCores = Integer.parseInt(serverRead.split(" ")[4]);
+				int minServerId = Integer.parseInt(serverRead.split(" ")[1]);
 
-						if (Integer.parseInt(serverIndiv[4]) > largestServerCores) {
-							largestServer = serverIndiv[0];
-							largestServerCores = Integer.parseInt(serverIndiv[4]);
+				for (int i = 0; i < serversPresentNo - 1; i++) {
+					serverRead = (String) dis.readLine();
 
-						}
+					String currentServerName = serverRead.split(" ")[0];
+					int currentServerCores = Integer.parseInt(serverRead.split(" ")[4]);
+					int currentServerId = Integer.parseInt(serverRead.split(" ")[1]);
 
+					if (currentServerCores >= neededCores && currentServerCores <= minServerCores) {
+						minServerCores = currentServerCores;
+						minServerName = currentServerName;
+						minServerId = currentServerId;
 					}
-
-					// System.out.println(largestServer);
-
-					// The following for loop finds out the number of servers present in the largest
-					// server type.
-					for (int i = 0; i < nRecs; i++) {
-						if (serversList[i][0].equals(largestServer)) {
-							numberOfLargeServers += 1;
-						}
-					}
-
-					dout.write(("OK\n").getBytes());
-					dis.readLine();
-					dout.flush();
 				}
+
+				dout.write(("OK\n").getBytes());
+				dis.readLine();
 
 				// Checks if the jobs data has the word JOBN in it. If it does, a job is
 				// scheduled to the largest server.
-				if (jobsIndivData[0].equals("JOBN")) {
-					String schedCommand = "SCHD " + jobId + " " + largestServer + " " + lastServerUsed;
-					// System.out.println(schedCommand);
-					dout.write((schedCommand + "\n").getBytes());
-					// System.out.println((String) dis.readLine() + "\n");
-					dis.readLine();
-					dout.flush();
-					lastServerUsed += 1;
-					if (lastServerUsed >= numberOfLargeServers) {
-						// System.out.println("Server count has been reset");
-						lastServerUsed = 0;
-					}
-				}
 
-				times += 1;
+				if (jobsIndivData[0].equals("JOBN")) {
+					String schedCommand = "SCHD " + jobId + " " + minServerName + " " + minServerId;
+					dout.write((schedCommand + "\n").getBytes());
+					dis.readLine();
+				}
 			}
 
 			dout.write(("QUIT\n").getBytes());
-			// System.out.println((String) dis.readLine());
 			dis.readLine();
 			dout.flush();
 
